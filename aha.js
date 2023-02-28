c27e4a82aeaeef5c00d17cb1e248d217e9582a69
Filemon Márk <filemonmark07@gmail.com>
2023-02-28 13:24:05 +0100

    pixel

---
+++ aha.js
@@ -0,0 +1,278 @@
+var pont=36
+
+function hide1() 
+{
+    circle1.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide2() 
+{
+    circle2.style.visibility="hidden";
+    pont-- 
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide3() 
+{
+    circle3.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide4() 
+{
+    circle4.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide5() 
+{
+    circle5.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide6() 
+{
+    circle6.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide7() 
+{
+    circle7.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide8() 
+{
+    circle8.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide9() 
+{
+    circle9.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide10() 
+{
+    circle10.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide11() 
+{
+    circle11.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide12() 
+{
+    circle12.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide13() 
+{
+    circle13.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide14() 
+{
+    circle14.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide15() 
+{
+    circle15.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide16() 
+{
+    circle16.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide1() 
+{
+    circle1.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide17() 
+{
+    circle17.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide18() 
+{
+    circle18.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide19() 
+{
+    circle19.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide20() 
+{
+    circle20.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide21() 
+{
+    circle21.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide22() 
+{
+    circle22.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide23() 
+{
+    circle23.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide24() 
+{
+    circle24.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide25() 
+{
+    circle25.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide26() 
+{
+    circle26.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide27() 
+{
+    circle27.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide28() 
+{
+    circle28.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide29() 
+{
+    circle29.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide30() 
+{
+    circle30.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide31() 
+{
+    circle31.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide32() 
+{
+    circle32.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide33() 
+{
+    circle33.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide34() 
+{
+    circle34.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide35() 
+{
+    circle35.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+function hide36() 
+{
+    circle36.style.visibility="hidden"; 
+    pont--
+    let ide=document.getElementById("ide")
+    ide.innerHTML=pont
+}
+
+
+
+function tippmix()
+{
+    let x="Arnold Schwarzenegger"
+    let tipp=document.getElementById("tipp").value
+
+    if(x==tipp)
+    {
+        alert("ELTALÁLTA! "+pont+" PONT")
+
+    }
+    else{
+        alert("NEM TALÁLTA EL!")
+    }
+}
